fc508f9fb0a45e9f980f0d979b8fa4d2a9a67cb4
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-13 17:59:12 +0300

    Creating React Fixed Navbar

---
+++ netflix/React Netflix Movie App Design Tutorial/src/App.jsx
@@ -0,0 +1,7 @@
+import Home from "./home/Home"
+import "./app.scss"
+
+const App = () => {
+  return <Home/>;
+};
+export default App;
---
+++ netflix/React Netflix Movie App Design Tutorial/src/home/Home.jsx
@@ -0,0 +1,27 @@
+import "./home.scss"
+import Navbar from "../components/navbar/Navbar"
+
+const Home = () => {
+    return (
+        <div className="home">    
+          <Navbar/>
+         <img src ="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+         width="100%"
+         alt=""
+         />
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sunt in, sed porro eaque hic quam temporibus tempore culpa. Repellat minus dicta suscipit assumenda dolores! Quia eveniet cupiditate dolorum nostrum.
+         Libero velit, aspernatur dicta hic nemo nulla sed, adipisci culpa officiis at in id excepturi ducimus impedit aut qui. Vitae, velit odio quam earum hic nobis saepe necessitatibus ullam? Ab.
+         Sit odio blanditiis deserunt, tempora illo porro saepe similique. Quis, earum repellendus veritatis aliquid iste minima, temporibus accusantium voluptatem cum, consequuntur expedita est aperiam aut! Molestiae adipisci soluta eum atque.
+         Error id quo hic autem dignissimos eaque nam expedita, rem dolorum ullam quae sit, perspiciatis veniam nemo distinctio doloremque veritatis animi! Natus blanditiis iste itaque temporibus nisi laboriosam non distinctio!
+         Voluptatem nihil enim, nemo magnam harum vero, veritatis nam beatae blanditiis temporibus sapiente? Harum ad alias quia cumque? Atque unde eligendi dignissimos possimus voluptatum consequuntur? Eum facere eos suscipit veritatis.
+         Velit minus eos id veritatis quam modi nisi natus placeat dignissimos expedita nam pariatur cumque maxime voluptate error qui ea quo accusantium ducimus iusto debitis, repellat illo est? Quae, et!
+         Unde eligendi illo alias error perferendis assumenda rerum excepturi vel quidem autem. Quo corporis voluptates culpa sit voluptas ipsa facere ipsum doloremque ab earum animi, dolor neque, debitis harum quaerat!
+         Dolorem at pariatur reiciendis eligendi earum alias non ut! Omnis adipisci, laborum repellat debitis ad dolorem doloremque quasi sit explicabo veniam molestias exercitationem? Perspiciatis, nemo mollitia blanditiis iure delectus assumenda!
+         Quas reprehenderit voluptatum atque, ut soluta repudiandae vitae fugit! Optio reprehenderit culpa laboriosam tenetur, voluptatibus libero rem voluptate! Quaerat voluptatum beatae esse maxime quas! Voluptate doloremque expedita assumenda hic aut?
+         Doloremque est cum perspiciatis error commodi, veniam culpa illo, quia aliquam nesciunt provident nulla quisquam recusandae ipsam libero vitae. Necessitatibus similique minima commodi voluptatibus quasi eaque blanditiis deserunt ea harum.
+       
+        </div>
+    )
+}
+
+export default Home

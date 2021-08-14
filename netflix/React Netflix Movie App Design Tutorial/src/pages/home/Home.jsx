123561c066bc00e75db695ab2f81c3d1e478e794
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-14 20:30:21 +0300

    React Register and Login Page Design

--- netflix/React Netflix Movie App Design Tutorial/src/pages/home/Home.jsx
+++ netflix/React Netflix Movie App Design Tutorial/src/pages/home/Home.jsx
@@ -1,19 +1,19 @@
-import "./home.scss"
-import Navbar from "../components/navbar/Navbar"
-import Featured from "../components/featured/Featured"
-import List from "../components/List"
+import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import "./home.scss";
+import List from "../../components/list/List";
 
 const Home = () => {
-    return (
-        <div className="home">    
-          <Navbar/> 
-         <Featured type="movie"/>
-         <List/>
-         <List/>
-         <List/>
-         </div>
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Home
+export default Home;

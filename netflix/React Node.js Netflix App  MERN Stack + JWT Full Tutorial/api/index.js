e6b26fc054c32519f04d5e74f102cd916094f9d8
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-18 22:19:09 +0300

    React Router DOM

--- netflix/React Node.js Netflix App  MERN Stack + JWT Full Tutorial/api/index.js
+++ netflix/React Node.js Netflix App  MERN Stack + JWT Full Tutorial/api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
 
 const app= express()
 
@@ -24,8 +25,9 @@ mongoose.connect(process.env.MONGO_URL, {
 
 
 app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
+app.use("/api/users",userRoute);listRoute
 app.use("/api/movies",movieRoute);
+app.use("/api/lists",listRoute);
 
 const server = process.env.PORT || 5000;
 
--- netflix/React Node.js Netflix App  MERN Stack + JWT Full Tutorial/client/src/components/navbar/Navbar.jsx
+++ netflix/React Node.js Netflix App  MERN Stack + JWT Full Tutorial/client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./navbar.scss";
 
 const Navbar = () => {
@@ -17,13 +18,13 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <span>Homepage</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New and Popular</span>
-          <span>My List</span>
-        </div>
-        <div className="right">
+          <Link  className="active" to="/"><span>Homepage</span></Link>
+          <Link  className="active" to="/series"><span>Series</span></Link>
+          <Link  className="active" to="/movies"><span>Movies</span></Link>
+          <Link  className="active" to="/"><span>New and Popular</span></Link>
+          <Link  className="active" to="/"> <span>My List</span></Link>
+       
+        </div>        <div className="right">
           <Search className="icon" />
           <span>KID</span>
           <Notifications className="icon" />
